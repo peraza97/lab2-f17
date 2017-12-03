8fa102077766c120d1fbbcc68ee5f44b6382706e
peraza97 <omarperaza19@gmail.com>
2017-12-03 07:04:55 +0000

    idk

--- shm.c
+++ shm.c
@@ -30,11 +30,42 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
 
-//you write this
-
-
-
-
+    //you write this
+    int i;
+    acquire(&(shm_table.lock));
+    for (i = 0; i< 64; i++) {
+ 
+    }
+    release(&(shm_table.lock));
+    /*    
+    //figure out if it was found or not
+    if(found){
+        //allocate variables needed for mappages
+        pde_t * pgdir = myproc()->pgdir;
+        uint va = PGROUNDUP(myproc()->sz);
+        char *mem = shm_table.shm_pages[i].frame; 
+        //mappages
+        mappages(pgdir,(char*)va, PGSIZE, V2P(mem), PTE_W|PTE_U);
+        //increment reference
+        myproc()->sz = va;
+        shm_table.shm_pages[i].refcnt +=1;
+        *pointer=(char *)va;
+    }
+    //not found 
+    else{
+        //initialize id and frame
+        shm_table.shm_pages[first].id = id;
+        char *frame = kalloc(); 
+        shm_table.shm_pages[first].frame = frame;
+        shm_table.shm_pages[first].refcnt = 1;
+        //mappages
+        pde_t * pgdir = myproc()->pgdir;
+        uint va = PGROUNDUP(myproc()->sz);
+        mappages(pgdir,(char*)va,PGSIZE,V2P(frame),PTE_W|PTE_U);
+        myproc()->sz = va;
+        *pointer=(char*)va;        
+    }
+*/
 return 0; //added to remove compiler warning -- you should decide what to return
 }
 
--- shm_cnt.c
+++ shm_cnt.c
@@ -19,16 +19,13 @@ struct shm_cnt *counter;
 //shm_open: first process will create the page, the second will just attach to the same page
 //we get the virtual address of the page returned into counter
 //which we can now use but will be shared between the two processes
-  
 shm_open(1,(char **)&counter);
- 
-//  printf(1,"%s returned successfully from shm_open with counter %x\n", pid? "Child": "Parent", counter); 
+  printf(1,"%s returned successfully from shm_open with counter %x\n", pid? "Child": "Parent", counter); 
   for(i = 0; i < 10000; i++)
     {
      uacquire(&(counter->lock));
      counter->cnt++;
      urelease(&(counter->lock));
-
 //print something because we are curious and to give a chance to switch process
      if(i%1000 == 0)
        printf(1,"Counter in %s is %d at address %x\n",pid? "Parent" : "Child", counter->cnt, counter);
